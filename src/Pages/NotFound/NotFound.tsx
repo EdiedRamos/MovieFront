92627e1d896db56fe3a4028e4b63966ff2e2502f
EdiedRamos <edieisa22@gmail.com>
2024-03-06 17:10:20 -0500

    style: update not found page style

--- src/Pages/NotFound/NotFound.tsx
+++ src/Pages/NotFound/NotFound.tsx
@@ -1,3 +1,34 @@
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
+
+// TODO: Add onclick callback
+
 export const NotFound = (): JSX.Element => {
-  return <div>NotFound</div>;
+  return (
+    <Box textAlign="center" py={10} px={6}>
+      <Heading
+        display="inline-block"
+        as="h2"
+        size="3xl"
+        bgGradient="linear(to-r, red.400, red.600)"
+        backgroundClip="text"
+      >
+        404
+      </Heading>
+      <Text fontSize="24px" mt={3} mb={2}>
+        Página no encontrada
+      </Text>
+      <Text fontSize="18px" color={"gray.500"} mb={6}>
+        La página que estás buscando parece no existir
+      </Text>
+      <Button
+        colorScheme="red"
+        bgGradient="linear(to-r, red.400, red.500, red.600)"
+        color="white"
+        variant="solid"
+        onClick={() => alert("not implemented")}
+      >
+        Ir a Inicio
+      </Button>
+    </Box>
+  );
 };
