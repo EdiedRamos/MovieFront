24f5965fe61f195c794b37f5ab0add20925863e4
EdiedRamos <edieisa22@gmail.com>
2024-03-06 12:49:55 -0500

    chore: add chakraui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import { Router } from "@/Routes";
 import { store } from "@/Store";
 
 export const App = () => {
   return (
-    <Provider store={store}>
-      <RouterProvider router={Router} />
-    </Provider>
+    <ChakraProvider>
+      <Provider store={store}>
+        <RouterProvider router={Router} />
+      </Provider>
+    </ChakraProvider>
   );
 };
 
