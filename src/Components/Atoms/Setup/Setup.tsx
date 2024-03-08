d2c6a533f2c04e13acc38b246cd8a9fc6d6a8d8b
EdiedRamos <edieisa22@gmail.com>
2024-03-08 18:06:26 -0500

    fix: load categories when the user is logged

--- src/Components/Atoms/Setup/Setup.tsx
+++ src/Components/Atoms/Setup/Setup.tsx
@@ -1,14 +1,24 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { verifyToken } from "@/Store/Session/sessionSlice";
 import { AppDispatch, setCategories } from "@/Store";
+import { RootState } from "@/Store";
 
 export const Setup = ({ children }: { children: JSX.Element }) => {
   const dispatch = useDispatch<AppDispatch>();
+
+  const sessionInfo = useSelector((state: RootState) => state.sessionReducer);
+
   useEffect(() => {
     dispatch(verifyToken());
-    dispatch(setCategories());
   }, [dispatch]);
+
+  useEffect(() => {
+    if (!sessionInfo.isCheckingToken && sessionInfo.isLogged) {
+      dispatch(setCategories());
+    }
+  }, [sessionInfo, dispatch]);
+
   return children;
 };
